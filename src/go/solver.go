a59c093cdf132b018549643d9b05d52e23ddf592
Giorgos Trogadas <trogadas@kth.se>
2021-06-20 20:28:27 +0200

    added skeleton files for the generator

---
+++ src/go/solver.go
@@ -0,0 +1,13 @@
+package main
+
+func valid_board() bool {
+	return true
+}
+
+func valid_move() bool {
+	return true
+}
+
+func solve() {
+	return
+}
---
+++ src/go/utils.go
@@ -0,0 +1,13 @@
+package main
+
+func load_data() {
+	return
+}
+
+func svae_data() {
+	return
+}
+
+func preprocess() {
+	return
+}
